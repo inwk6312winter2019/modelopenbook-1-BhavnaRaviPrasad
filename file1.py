a751be0d3b3064235a1d162cf19e04f92e599638
Bhavna <bh951651@dal.ca>
2019-02-06 18:49:53 +0000

    final

--- file1.py
+++ file1.py
@@ -16,3 +16,45 @@ def list_ifname_ip(y):
 
 print(list_ifname_ip(y))
 
+def replace_str():
+    myfile = open("running-config.cfg","r")
+    newfile = open("running-config-new.cfg","w")
+    for my in myfile:
+        mylist = my.split()
+        if "255.255.0.0" in my:
+            line = my.replace("255.255.0.0","255.0.0.0")
+            newfile.write(line)
+        elif "255.255.255.0" in my:
+            line  = my.replace("255.255.255.0","255.0.0.0")
+            newfile.write(line)
+        elif "172" in my:
+            line = my.replace("172","10")
+            newfile.write(line)
+        elif "192" in my:
+            line = my.replace("192","10")
+            newfile.write(line)
+        else:
+            newfile.write(my)
+print(replace_str())
+
+def create_list():
+    myfile = open("running-config.cfg","r")
+    transit_access_in = []
+    fw_management_access_in = []
+    global_access = []
+    for my in myfile:
+        if "transit_access_in" in my:
+            my = my.strip()
+            transit_access_in.append(my)
+        if "fw-management_access_in" in my:
+            my = my.strip()
+            fw_management_access_in.append(my)
+        if "global_access" in my:
+            my = my.strip()
+            global_access.append(my)
+
+    print("\ntransit_access list:",transit_access_in)
+    print("\nfw_management_list:",fw_management_access_in)
+    print("\nglobal_access list:",global_access)
+
+create_list()
